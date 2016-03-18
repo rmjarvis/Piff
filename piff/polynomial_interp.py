fcfb9ce5400b801712738f6af0e52e89e1001403
Joe Zuntz <joezuntz@googlemail.com>
2016-03-18 17:06:03 +0000

    Update the polynomial interpolator to keep up with rest of code and with some simple tests

--- piff/polynomial_interp.py
+++ piff/polynomial_interp.py
@@ -19,52 +19,254 @@
 from __future__ import print_function
 
 from .interp import Interp
+import numpy
+from numpy.polynomial.polynomial import polyval2d
 
 class Polynomial(Interp):
     """
-    Uses the scipy curve_fit command to fit a polynomial surface
+    An interpolator that uses  scipy curve_fit command to fit a polynomial 
+    surface to each parameter passed in independently.
+
+    TODO: This code is written so we can refactor to pull out the general
+    bits that allow you to use curve_fit from the specific polynomial model
+    used here.
     """
     def __init__(self, order):
         self.order=order
+        #The total number of variables per parameters.
+        #We want to go up to a maximum total power of
+        #"order", so 
+
+        #Before any fitting is done we just None for the
+        #coeffs
+        self.coeffs = None
+        self.generate_indices()
+        self.nvariable=len(self.indices)
+
+    def generate_indices(self):
+        """Generate, for internal use, the exponents i,j used in the polynomial model
+        p(u,v) = sum c_{ij} u^i v^j
+
+        This needs to be called whenever the order of the polynomial fit is 
+        changed. At the moment that is just when an object is initialized or
+        updated from file.
+         """
+        indices = []
+        for p in xrange(self.order+1):
+            for i in xrange(p+1):
+                j = p-i
+                indices.append((i,j))
+        self.indices = indices
+
+    def pack_coefficients(self, C):
+        """Pack the 2D matrix of coefficients used as the model fit parameters
+        into a vector of coefficients, either so this can be passed as a starting
+        point into the curve_fit routine or for serialization to file.
+
+        For subclasses, the 2D matrix format could be whatever you wanted as long
+        as initialGuess, interpolationModel, and the pack and unpack functions are 
+        consistent. The intialGuess method can return and the interpolationModel can
+        accept parameters in whatever form you like (e.g. could be a dict if you want)
+        as long as pack_coefficients can convert this into a 1D array and unpack_coefficients
+        convert it the other way.
+
+        :param C:          A 2D matrix of polynomial coefficients in the form that
+                           the numpy polynomial form is expecting:
+                           p(x,y,c) = sum_{i,j} c_{ij} x^i y^j
+        :returns coeffs:    A 1D numpy array of coefficients of length self.nvariable
+        """
+        coeffs = numpy.zeros(self.nvariable)
+        for k,(i,j) in enumerate(self.indices):
+            coeffs[k] = C[i,j]
+        return coeffs
+
+
+    def unpack_coefficients(self, coeffs):
+        """Unpack a sequence of parameters into the 2D matrix.
+
+        This function is the inverse of pack_coefficients
+                           
+        :param coeffs:     A 1D numpy array of coefficients  of length self.nvariable
+        :returns:          A 2D matrix of polynomial coefficients in the form that
+                           the numpy polynomial form is expecting:
+                           p(x,y,c) = sum_{i,j} c_{ij} x^i y^j
+
+        """
+        k=0
+        C = numpy.zeros((self.order+1, self.order+1))
+        for k,(i,j) in enumerate(self.indices):
+                C[i,j] = coeffs[k]
+                k+=1
+        return C
+
     
-    def _fit_function(self, xy, *coeffs):
-        # x is the data values - the coordinates of the stars
-        # shape (2,nstar)
-        import numpy
-        x = xy[0]
-        y = xy[1]
-        px = coeffs[:self.order]
-        py = coeffs[self.order:]
-        f = numpy.polyval(px, x) * numpy.polyval(py, y)
+    def interpolationModel(self, pos, C):
+        """Generate the polynomial variation of some quantity at x and y
+        coordinates for a given coefficient matrix.
+
+        TODO: At the moment this function is expecting a numpy array of
+        shape (2,nstar) for the positions. We might want to use a galsim
+        position object instead since I think some code elsewhere in Piff
+        is expecting this.
+
+        This is an internal method used during the fitting.
+        :param pos:         A numpy array of the u,v positions at which to build 
+                           the model
+        :param C:          A 2D matrix of polynomial coefficients in the form that
+                           the numpy polynomial form is expecting:
+                           p(x,y,c) = sum_{i,j} c_{ij} x^i y^j
+                           
+        :returns:          A numpy array of the calculated p_x(x)*p_y(y) where 
+                           the p functions are polynomials.
+
+        """
+        u = pos[0]
+        v = pos[1]
+        f = polyval2d(u, v, C)
         return f
 
-    def fitData(self, data, pos):
-        """Fit for the interpolation coefficients given some data.
+    def initialGuess(self, positions, parameter):
+        """Make an initial guess for a set of parameters
+        to use in the fit for your model. This is passed
+        to curve_fit as a starting point.
 
-        Here the "fit" is just the mean.
+        :param positions:  A list of positions ((u,v) in this case) of stars.
+        :param parameter:  A numpy array of the measured values of a parameter
+                           for each star
+                           
+        :returns:          A guess for the parameters. In this case a 2D matrix
+                           which is zero everywhere except for (0,0).  This should
+                           correspond to a flat function of the parameters with 
+                           value given by the mean.
 
-        :param data:        A list of lists of data vectors (numpy arrays) for each star
-        :param pos:         A list of lists of positions of the stars
+
+        """        
+        #We need a starting point for the fitter.
+        #Use a constant value over the whole field as 
+        #a guess.
+        C = numpy.zeros((self.order+1, self.order+1))
+        C[0,0] = parameter.mean()
+        return C
+
+
+
+    def solve(self, pos, vectors, logger=None):
+        """Solve for the interpolation coefficients given some data.
+
+        This currently assumes that our positions pos are just u and v.
+
+        :param pos:         A list of positions to use for the interpolation.
+        :param vectors:     A list of parameter vectors (numpy arrays) for each star.
+        :param logger:      A logger object for logging debug info. [default: None]
         """
-        import numpy
         import scipy.optimize
-        data = numpy.array(data).T
-        P = []
-        X = numpy.array([p.x for p in pos])
-        Y = numpy.array([p.y for p in pos])
-        positions = numpy.array([X,Y])
+
+        #We will want to index things later, so useful
+        #to convert these to numpy arrays and transpose
+        #them to the order we need.
+        parameters = numpy.array(vectors).T
+        positions = numpy.array(pos).T
        
-        #We fit a separate polynomial in each parameter
-        for parameter in data:
-            p0 = numpy.zeros(self.order*2)
-            p0[0] = parameter.mean()
-            p0[self.order] = parameter.mean()
-            p,covmat=scipy.optimize.curve_fit(self._fit_function, positions, parameter, p0)
-            P.append(p)
-        self.coeffs = numpy.concatenate(P)
+        #It seems like we can't get the number of parameters (which
+        #depends on what model we have fitted) before we get to this
+        # point. That's a little awkward.
+        self.nparam = len(parameters)
 
-    def getParameters(self):
-        return self.coeffs
+        coeffs = []
 
-    def interpolate(self, image_num, pos):
-        raise ValueError("Not done this bit yet")
+        #This model function adapts our interpolationModel method
+        #into the form that the scipy curve_fit function is expecting.
+        #It just needs to unpack a linear exploded list of coefficients
+        #into the matrix form that interpolationModel wants.
+        def model(uv,*coeffs):
+            C = self.unpack_coefficients(coeffs)
+            return self.interpolationModel(uv,C)
+
+
+        #Loop through the parameters
+        for p, parameter in enumerate(parameters):
+            #To replace the polynomial function in this code
+            #with another model it should only be necessary to 
+            #override the methods, initial_guess, fit_function,
+            #and perhaps the pack and unpack methods
+            p0 = self.pack_coefficients(self.initialGuess(positions, parameter))
+            # Black boxes curve fitter from scipy!
+            # We may want to look into the tolerance and other parameters
+            # of this function.
+            p,covmat=scipy.optimize.curve_fit(model, positions, parameter, p0)
+            
+            #Build up the list of outputs, one for each parameter
+            coeffs.append(self.unpack_coefficients(p))
+
+        #Each of these is now a list of length nparam, each element
+        #of which is a 2D array of coefficients to the corresponding 
+        #exponents. Where "corresponding" is as-defined in 
+        #self.unpack_coefficients
+        self.coeffs = coeffs
+
+    def writeSolution(self, fits, extname):
+        """Read the solution from a FITS binary table.
+
+        We save two columns for the exponents and one column
+        of coefficients for each parameter.
+
+        :param fits:        An open fitsio.FITS object.
+        :param extname:     The name of the extension with the interp information.
+        """
+
+
+        #First the exponents - we need arrays of the indices,
+        #which we get with this piece of zip oddness which unzips
+        #a list of pairs into a pair of lists
+        cols = zip(*self.indices)
+        #We also need to tell fitsio the data types of these.
+        dtypes = [ ('u_exponent', int), ('v_exponent', int) ]
+
+        #We will also need one column per parameter that we have
+        #fit containing the coefficients for that parameter
+        for p in xrange(self.nparam):
+            dtypes.append(('coeff_{}'.format(p),float))
+            #The pack function converts our format (which in 
+            # this case is a 2D matrix) into a linear one.
+            col = self.pack_coefficients(self.coeffs[p])
+            cols.append(col)
+
+        #We will need some more identifying information that this!
+        #I would suggest some kind of standard piff
+        header={"NPARAM":self.nparam, "NVAR":self.nvariable, "ORDER":self.order}
+        data = numpy.array(zip(*cols), dtype=dtypes)
+        fits.write_table(data, extname=extname, header=header)
+
+    def readSolution(self, fits, extname):
+        """Read the solution from a FITS binary table.
+
+        The extension should contain the same values as are saved
+        in the writeSolution method.
+
+        :param fits:        An open fitsio.FITS object.
+        :param extname:     The name of the extension with the interp information.
+        """
+        header = fits[extname].read_header()
+        data = fits[extname].read()
+
+        self.order = header['ORDER']
+        self.nvariable = header['NVAR']
+        self.nparam = header['NPARAM']
+        self.coeffs = []
+        self.generate_indices()
+
+        for i in xrange(self.nparam):
+            col = data["coeff_{}".format(i)]
+            self.coeffs.append(self.unpack_coefficients(col))
+
+
+    def interpolate(self, pos, logger=None):
+        """Perform the interpolation to find the interpolated parameter vector at some position.
+
+        :param pos:         The position to which to interpolate.
+        :param logger:      A logger object for logging debug info. [default: None]
+
+        :returns: the parameter vector (a numpy array) interpolated to the given position.
+        """
+        p = [self.interpolationModel(pos, self.coeffs[i]) for i in xrange(self.nparam)]
+        return numpy.array(p)
---
+++ tests/test_poly.py
@@ -0,0 +1,264 @@
+import numpy
+import piff
+
+def test_poly_indexing():
+    #Some indexing tests for a polynomial up to order 3
+    N = 3
+    interp = piff.Polynomial(N)
+
+    #We expect there to be these coefficients:
+    # x^0 y^0   1
+
+    # x^0 y^1   2
+    # x^1 y^0   3
+
+    # x^0 y^2   4
+    # x^1 y^1   5 
+    # x^2 y^0   6
+
+    # x^0 y^3   7
+    # x^1 y^2   8
+    # x^2 y^1   9
+    # x^3 y^0   10 
+
+    #Check that we have the indices we expect
+    assert interp.indices == [
+        (0,0),
+        (0,1), (1,0),
+        (0,2), (1,1), (2,0),
+        (0,3), (1,2), (2,1), (3,0)
+    ]
+    assert interp.nvariable==10
+
+    #check the packing then unpacking a 
+    packed = numpy.random.uniform(size=interp.nvariable)
+    unpacked = interp.unpack_coefficients(packed)
+    packed_test = interp.pack_coefficients(unpacked)
+
+    #Check that the shape is 4*4 in the unpacked (because we
+    #want space for all the terms), and that we can unpack and
+    #repack successfully.
+    assert (packed==packed_test).all()
+    assert unpacked.shape == (N+1,N+1)
+
+    unpacked_test = numpy.zeros_like(unpacked)
+
+    #check that we have zeros for the terms that should be zero in the matrix.
+    #We don't want any terms with total exponent > N.
+    for i in xrange(N+1):
+        for j in xrange(N+1):
+            if i+j>3:
+                assert unpacked[i,j]==0.0
+                unpacked_test[i,j]=0.0
+
+    #Now do the test the other way around, checking that 
+    #we can pack and then unpack
+    packed_test_2 = interp.pack_coefficients(unpacked_test)
+    unpacked_test_2 = interp.unpack_coefficients(packed_test_2)
+    assert (unpacked_test_2 == unpacked_test).all()
+
+def test_poly_interp():
+    #Zero'th order polynomial fitting should be pretty trivial, just
+    #the same as the mean fitting. So much of this code is just taken from
+    #the mean testing in test_simple
+    N = 0
+    interp = piff.Polynomial(N)
+    nstars = 100
+    nparam = 5
+
+    #Choose some random values of star parameters
+    vectors = [ numpy.random.random(size=nparam) for i in range(nstars) ]
+
+    #take the mean of them. Our curve fit should be able to reproduce this.
+    mean = numpy.mean(vectors, axis=0)
+
+    #Choose some random positions in the field.
+    #NB think more about bounds and 
+    pos = [ (numpy.random.random()*10, numpy.random.random()*10)
+            for i in range(nstars) ]
+
+    # Run our solver.
+    interp.solve(pos, vectors)
+
+    #we expect one set of coefficients per object
+    assert len(interp.coeffs)==5
+
+    #We should have very close values (not necessarily identical) since
+    #we calculate these in numerically different ways.
+    for mu, val in zip(mean, interp.coeffs):
+        assert numpy.isclose(mu, val[0,0])
+
+    #We also expect that if we interpolate to any point we just
+    #get the mean as well
+    for i in xrange(30):
+        p=(numpy.random.random()*10, numpy.random.random()*10)
+        v = interp.interpolate(p)
+        assert numpy.allclose(v, mean)
+
+
+def test_poly_linear():
+    #Now lets do something more interesting - test a linear model.
+    #with no noise this should fit really well, though again not
+    #numerically perfectly.
+    numpy.random.seed(1234)
+    nparam = 3
+    N = 1
+    nstars=50   
+    interp = piff.Polynomial(N)
+    X = 10.0 #size of the field
+    Y = 10.0
+
+    pos = [ (numpy.random.random()*X, numpy.random.random()*Y)
+            for i in range(nstars) ]
+
+
+    #Let's make a function that is linear just as a function of one parameter
+    #These are the linear fit parameters for each parameter in turn
+    m1 = numpy.random.uniform(size=nparam)
+    m2 = numpy.random.uniform(size=nparam)
+    c = numpy.random.uniform(size=nparam)
+    def f(pos):
+        u = pos[0]
+        v = pos[1]
+        r = m1*u+m2*v+c
+        return r
+
+    #Simulate the vectors under this model
+    vectors = [f(p) for p in pos]
+
+    #Fit them. Linear fitting is quite easy so this should 
+    #be okay
+    interp.solve(pos, vectors)
+
+    #Check that the interpolation recovers the desired function
+    for i in xrange(30):
+        p=(numpy.random.random()*X, numpy.random.random()*Y)
+        assert numpy.allclose(f(p), interp.interpolate(p))
+
+
+def test_poly_quadratic():
+    #This is basically the same as linear but with
+    #quadratic variation
+    numpy.random.seed(1234)
+    nparam = 3
+    N = 2
+    nstars=50   
+    interp = piff.Polynomial(N)
+    X = 10.0 #size of the field
+    Y = 10.0
+
+    pos = [ (numpy.random.random()*X, numpy.random.random()*Y)
+            for i in range(nstars) ]
+
+
+    #Let's make a function that is linear just as a function of one parameter
+    #These are the linear fit parameters for each parameter in turn
+    m1 = numpy.random.uniform(size=nparam)
+    m2 = numpy.random.uniform(size=nparam)
+    q1 = numpy.random.uniform(size=nparam)
+    c = numpy.random.uniform(size=nparam)
+    def f(pos):
+        u = pos[0]
+        v = pos[1]
+        r = q1*u*v+ m1*u+m2*v+c
+        return r
+
+    #Simulate the vectors under this model
+    vectors = [f(p) for p in pos]
+
+    #Fit them. Linear fitting is quite easy so this should 
+    #be okay
+    interp.solve(pos, vectors)
+
+    #Check that the interpolation recovers the desired function
+    for i in xrange(30):
+        p=(numpy.random.random()*X, numpy.random.random()*Y)
+        assert numpy.allclose(f(p), interp.interpolate(p))
+
+def test_poly_guess():
+    #test that our initial guess gives us a flat function given
+    #by the mean
+    numpy.random.seed(12434)
+    nparam = 10
+    N = 2
+    X = 10.0
+    Y = 10.0
+    nstars=50   
+    interp = piff.Polynomial(N)
+    pos = [ (numpy.random.random()*X, numpy.random.random()*Y)
+            for i in range(nstars) ]
+
+    for i in xrange(nparam):
+        param = numpy.random.random(size=nstars)
+        p0 = interp.initialGuess(pos, param)
+        mu = param.mean()
+        assert numpy.isclose(p0[0,0],mu)
+        assert (p0[0,1:]==0.0).all()
+        assert (p0[1,0]==0.0).all()
+        assert (p0[1:,1:]==0.0).all()
+        assert numpy.allclose(interp.interpolationModel(pos, p0), mu)
+
+
+
+def test_poly_load_save():
+    #Test that we can serialize and deserialize a polynomial 
+    #interpolator correctly.  Copying all this stuff from above:
+
+    numpy.random.seed(12434)
+    nparam = 3
+    N = 2
+    nstars=50   
+    interp = piff.Polynomial(N)
+    X = 10.0 #size of the field
+    Y = 10.0
+
+    pos = [ (numpy.random.random()*X, numpy.random.random()*Y)
+            for i in range(nstars) ]
+
+    #Let's make a function that is linear just as a function of one parameter
+    #These are the linear fit parameters for each parameter in turn
+    m1 = numpy.random.uniform(size=nparam)
+    m2 = numpy.random.uniform(size=nparam)
+    q1 = numpy.random.uniform(size=nparam)
+    c = numpy.random.uniform(size=nparam)
+
+    def f(pos):
+        u = pos[0]
+        v = pos[1]
+        r = q1*u*v+ m1*u+m2*v+c
+        return r
+
+    #Simulate the vectors under this model
+    vectors = [f(p) for p in pos]
+
+    #Fit them. Linear fitting is quite easy so this should 
+    #be okay
+    interp.solve(pos, vectors)
+
+    #We should overwrite the order parameter when we load in
+    interp2 = piff.Polynomial(0)
+
+    import tempfile
+    import os
+    import fitsio
+    extname = "interp"
+    dirname = tempfile.mkdtemp()
+    filename=os.path.join(dirname,'poly_test_file.fits')
+    with fitsio.FITS(filename,'rw',clobber=False) as f:
+        interp.writeSolution(f, extname=extname)
+    f2 = fitsio.FITS(filename, "r")
+    interp2.readSolution(f2, extname=extname)
+    os.remove(filename)
+    os.rmdir(dirname)
+
+
+    assert interp2.order==N
+    assert interp2.nvariable==interp.nvariable
+    assert interp2.indices==interp.indices
+
+    #Check that the old and new interpolators generate the same
+    #value
+    for i in xrange(30):
+        p=(numpy.random.random()*X, numpy.random.random()*Y)
+        assert numpy.allclose(interp.interpolate(p),interp2.interpolate(p))
+
