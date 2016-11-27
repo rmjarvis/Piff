958cfb51b027e21d42c458b97d36694beccc113a
Mike Jarvis <michael@jarvis.net>
2016-11-27 13:15:53 -0500

    Clean up spacing in decam_wavefront.py

--- piff/des/decam_wavefront.py
+++ piff/des/decam_wavefront.py
@@ -29,21 +29,29 @@ class DECamWavefront(kNNInterp):
     """
 
     def __init__(self, file_name, extname, n_neighbors=15, weights='uniform', algorithm='auto',
-                 p=2,logger=None):
-        """Load up a fits file containing the optics model and use it to build the wavefront interpolator
+                 p=2, logger=None):
+        """Load up a fits file containing the optics model and use it to build the wavefront
+        interpolator
 
         :param file_name:   Fits file containing the wavefront
         :param extname:     Extension name
         :param n_neighbors: Number of neighbors used for interpolation. [default: 15]
-        :param weights:     Weight function used in prediction. Possible values are 'uniform', 'distance', and a callable function which accepts an array of distances and returns an array of the same shape containing the weights. [default: 'uniform']
-        :param algorithm:   Algorithm used to compute nearest neighbors. Possible values are 'ball_tree', 'kd_tree', 'brute', and 'auto', which tries to determine the best choice. [default: 'auto']
-        :param p:           Power parameter of distance metrice. p=2 is default euclidean distance, p=1 is manhattan. [default: 2]
+        :param weights:     Weight function used in prediction. Possible values are 'uniform',
+                            'distance', and a callable function which accepts an array of distances
+                            and returns an array of the same shape containing the weights.
+                            [default: 'uniform']
+        :param algorithm:   Algorithm used to compute nearest neighbors. Possible values are
+                            'ball_tree', 'kd_tree', 'brute', and 'auto', which tries to determine
+                            the best choice. [default: 'auto']
+        :param p:           Power parameter of distance metrice. p=2 is default euclidean distance,
+                            p=1 is manhattan. [default: 2]
         :param logger:      A logger object for logging debug info. [default: None]
         """
 
         self.z_min = 4
         self.z_max = 11
-        # these were kwargs in knn interp, but are no longer kwargs because they are fixed by our wavefront model!
+        # these were kwargs in knn interp, but are no longer kwargs because they are fixed by our
+        # wavefront model!
         self.attr_interp = ['focal_x', 'focal_y']
         self.attr_target_wavefront = ['z{0}'.format(zi) for zi in range(self.z_min, self.z_max + 1)]
         self.attr_target = range(0, self.z_max + 1 - self.z_min)
@@ -105,15 +113,17 @@ class DECamWavefront(kNNInterp):
                     nu_misalignment[indx, 2] = misalignment[key]
             misalignment = nu_misalignment
         # if array, check that shape is right, and put it in
-        if hasattr(self, 'misalignment'):
-            assert misalignment.shape == self.misalignment.shape,"New misalignment shape must match old!"
+        if hasattr(self, 'misalignment') and misalignment.shape != self.misalignment.shape:
+            raise ValueError("New misalignment shape must match old!")
         self.misalignment = misalignment
 
     def _predict(self, locations, targets=None, logger=None):
         """Predict from knn.
 
-        :param locations:   The locations for interpolating. (n_samples, n_features). In sklearn parlance, this is 'X'
-        :param targets:   The target values. (n_samples, n_targets). In sklearn parlance, this is 'y'. If given, then only apply misalignment
+        :param locations:   The locations for interpolating. (n_samples, n_features). In sklearn
+                            parlance, this is 'X'
+        :param targets:     The target values. (n_samples, n_targets). In sklearn parlance, this is
+                            'y'. If given, then only apply misalignment
 
         :returns:   Regressed parameters targets (n_samples, n_targets)
         """
