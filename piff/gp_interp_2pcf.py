fiwed what was wrong in testing gp with spatial average

piff/gp_interp_2pcf.py
@@ -270,23 +270,11 @@ class GPInterp2pcf(Interp):
                 self.kernels = [copy.deepcopy(ker) for ker in self.kernel_template]                                        
         return stars
 
-    def _build_average(self, X, gp=True):
+    def _build_average(self, X):
         if np.sum(np.equal(self._X0, 0)) != len(self._X0[:,0])*len(self._X0[0]):
-            if not gp:
-                neigh = KNeighborsRegressor(n_neighbors=4)
-                neigh.fit(self._X0, self._y0)
-                average = neigh.predict(X)
-            else:
-                self._kernels0 = copy.deepcopy(self.kernels)
-                for i in range(self.nparams):
-                    y_err = np.ones_like(self._y0[:,i])*(np.std(self._y0[:,i])/100.)
-                    self._kernels0[i] = self._fit(self._kernels0[i],
-                                                  self._X0, self._y0[:,i],
-                                                  y_err)
-                average = np.array([self.return_gp_predict(self._y0[:,i],
-                                                           self._X0, X, ker,
-                                                           y_err=np.ones_like(self._y0[:,i])*(np.std(self._y0[:,i])/100.))
-                                    for i, ker in enumerate(self._kernels0)]).T
+            neigh = KNeighborsRegressor(n_neighbors=4)
+            neigh.fit(self._X0, self._y0)
+            average = neigh.predict(X)            
             return average
         else:
             return np.zeros((len(X[:,0]), self.nparams))
@@ -325,7 +313,7 @@ class GPInterp2pcf(Interp):
             #self._spatial_average = self._pca.transform(self._spatial_average)
 
         if self.normalize:
-            self._mean = np.mean(y,axis=0)
+            self._mean = np.mean(y - self._spatial_average, axis=0)
         else:
             self._mean = np.zeros(self.nparams)
 

tests/test_meanify.py
@@ -85,7 +85,7 @@ def params_to_stars(params, noise=0.0, rng=None):
 
 def make_average(Coord=None, gp=True):
     if Coord is None:
-        x = np.linspace(0, 200, 10)
+        x = np.linspace(0, 2048, 10)
         x, y = np.meshgrid(x,x)
         x = x.reshape(len(x)**2)
         y = y.reshape(len(y)**2)
@@ -93,7 +93,7 @@ def make_average(Coord=None, gp=True):
         x = Coord[:,0]
         y = Coord[:,1]
     
-    average = 0.02 + 5e-8*(x-1024)**2 + 5e-8*(y-1024)**2
+    average = 0.02 + 7e-8*(x-1024)**2 + 7e-8*(y-1024)**2
     params = np.recarray((len(x),), dtype=star_type)
 
     keys = ['hlr', 'g1', 'g2']
@@ -107,7 +107,7 @@ def make_average(Coord=None, gp=True):
     if gp:
         from scipy.spatial.distance import pdist, squareform
         dists = squareform(pdist(np.array([x, y]).T))
-        cov = 0.05**2 * np.exp(-0.5*dists**2/30.**2) 
+        cov = 0.05**2 * np.exp(-0.5*dists**2/300.**2) 
         
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
@@ -132,14 +132,14 @@ def make_average(Coord=None, gp=True):
 
 def setup():
     np.random.seed(42)
-    for im in range(30):
+    for im in range(20):
         print(im)
         image = galsim.Image(2048, 2048, scale=0.26)
 
         x_list = [np.random.uniform(0, 2048)]
         y_list = [np.random.uniform(0, 2048)]
         i=0
-        while i < 599:
+        while i < 549:
             x = np.random.uniform(0, 2048)
             y = np.random.uniform(0, 2048)
             D = np.sqrt((np.array(x_list)-x)**2 + (np.array(y_list)-y)**2)
@@ -200,7 +200,7 @@ def test_meanify():
     config = {
         'input' : {
             'file_name' : psf_file,
-            'binning' : 30,
+            'binning' : 20,
         },
         'output' : {
             'file_name' : average_file,
@@ -213,28 +213,60 @@ def test_meanify():
     keys = ['hlr', 'g1', 'g2']
     import pylab as plt
     for i,key in enumerate(keys):
-        #plt.hist(params0[key] - average['PARAMS0'][0][:,i],np.linspace(-0.01,0.01,30))
-        np.testing.assert_allclose(params0[key], average['PARAMS0'][0][:,i], rtol=1e-1, atol=1e-3)
-        #plt.show()
+        plt.hist(params0[key] - average['PARAMS0'][0][:,i],np.linspace(-0.02,0.02,30))
+        #np.testing.assert_allclose(params0[key], average['PARAMS0'][0][:,i], rtol=1e-1, atol=1e-2)
+        plt.show()
 
 if __name__ == '__main__':
 
-    #test_meanify()
+    #tt = make_average(gp = True)
+    test_meanify()
+    #def _getcoord(star):
+    #    return np.array([star.data[key] for key in ['u','v']])
 
-    x = np.random.uniform(0, 2048, size=700)
-    y = np.random.uniform(0, 2048, size=700)
-    coord = np.array([x,y]).T
-    average = make_average(Coord=coord)
-    
-    stars = params_to_stars(average, noise=0.0, rng=None)
-    stars_training = stars[:600]
-    stars_validation = stars[600:]
+    #x = np.random.uniform(0, 2048, size=1000)
+    #y = np.random.uniform(0, 2048, size=1000)
+    #coord = np.array([x,y]).T
+    #average = make_average(Coord=coord)
 
-    gp = piff.GPInterp2pcf(kernel="0.05**2 * RBF(30.*0.26)",
-                           optimize=False, white_noise=1e-5, average_fits='output/average.fits')
-    gp.initialize(stars_training)
-    gp.solve(stars_training)
-    stars_interp = gp.interpolateList(stars_validation)
+    #stars = params_to_stars(average, noise=0.0, rng=None)
+    #stars_training = stars[:900]
+    #stars_validation = stars[900:]
 
-    params_interp = np.array([s.fit.params for s in stars_interp])
-    params_validation = np.array([s.fit.params for s in stars_validation])
+    #gp = piff.GPInterp2pcf(kernel="0.0025 * RBF(300.*0.26)",
+    #                       optimize=False, white_noise=1e-3, average_fits='output/average.fits')
+    #gp.initialize(stars_training)
+    #gp.solve(stars_training)
+    #stars_interp = gp.interpolateList(stars_validation)
+
+    #params_interp = np.array([s.fit.params for s in stars_interp])
+    #params_validation = np.array([s.fit.params for s in stars_validation])
+    #params_training = np.array([s.fit.params for s in stars_training])
+
+    #Xt = np.array([_getcoord(s) for s in stars_training])
+    #Xv = np.array([_getcoord(s) for s in stars_validation]) 
+
+    #import pylab as plt
+
+    #for i in range(3):
+    #    plt.figure()
+    #    plt.hist(params_validation[:,i]-np.mean(params_validation[:,i]),bins=np.linspace(-0.20,0.20,30),histtype='step',color='blue',lw=3)
+    #    plt.hist((params_validation[:,i] - params_interp[:,i]),bins=np.linspace(-0.02,0.02,30),histtype='step',color='k',lw=3)
+
+    #for i in range(3):
+    #    MIN = np.min(params_training[:,i])
+    #    MAX = np.max(params_training[:,i])
+    #    plt.figure(figsize=(15,4))
+    #    plt.subplots_adjust(left=0.05,right=0.95)
+    #    plt.subplot(1,4,1)
+    #    plt.scatter(Xt[:,0],Xt[:,1], c= params_training[:,i],lw=0,s=40, vmin=MIN, vmax=MAX)
+    #    plt.colorbar()
+    #    plt.subplot(1,4,2)
+    #    plt.scatter(Xv[:,0],Xv[:,1], c= params_validation[:,i],lw=0,s=40,vmin=MIN, vmax=MAX)
+    #    plt.colorbar()
+    #    plt.subplot(1,4,3)
+    #    plt.scatter(Xv[:,0],Xv[:,1], c= params_interp[:,i],lw=0,s=40,vmin=MIN, vmax=MAX)
+    #    plt.colorbar()
+    #    plt.subplot(1,4,4)
+    #    plt.scatter(Xv[:,0],Xv[:,1], c= params_validation[:,i] - params_interp[:,i],lw=0,s=40)#,vmin=MIN, vmax=MAX)
+    #    plt.colorbar()