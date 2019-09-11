e973767965ada27c817590a90ff132763178e576
Mike Jarvis <michael@jarvis.net>
2019-09-11 15:01:35 -0400

    Move test_lmfit_errors to devel directory

--- tests/test_gsobject_model.py
+++ tests/test_gsobject_model.py
@@ -681,50 +681,6 @@ def test_direct():
             roundtrip_model = piff.GSObjectModel.read(f, 'psf_model')
         assert model.__dict__ == roundtrip_model.__dict__
 
-@timer
-def test_lmfit_errors():
-    import lmfit
-    Nsamples = 300
-    for force_model_center in [True]:
-        for include_pixel in [True]:
-            for model_name, model_init in zip(['Kolmogorov', 'Gaussian'], [piff.Kolmogorov, piff.Gaussian]):
-                for noise in [1e-4, 1e-3]:
-                    model = model_init(fastfit=False, force_model_center=force_model_center, include_pixel=include_pixel)
-
-                    params = np.array([0.2, -0.3, 1.2, -0.05, 0.07])
-                    if force_model_center:
-                        params = params[2:]
-
-                    params_out = []
-                    errors_out = []
-                    for i in range(Nsamples):
-                        star = piff.Star(model.draw(piff.Star(piff.Star.makeTarget(x=0, y=0, scale=0.263, stamp_size=32).data, piff.StarFit(params))).data, None)
-                        # add noise
-                        star.weight.fill(1. / noise ** 2)
-                        gn = galsim.GaussianNoise(sigma=noise, rng=None)
-                        star.image.addNoise(gn)
-
-                        # fit
-                        star = model.initialize(star)
-                        # TODO: with PFL's PR, the errors will be a property of the fit
-                        lmparams = model._lmfit_params(star)
-                        results = model._lmfit_minimize(lmparams, star)
-                        flux, du, dv, scale, g1, g2 = results.params.valuesdict().values()
-                        # only care about the shape terms, really
-                        params_out.append(np.array([scale, g1, g2]))
-                        error = np.sqrt(np.diag(results.covar)[3:])
-                        errors_out.append(error)
-                    params_out = np.array(params_out)
-                    errors_out = np.array(errors_out)
-                    # rough estimate of whether errors are working as designed
-                    pull = ((params_out - params) / errors_out)
-                    pull_mean = np.mean(pull, axis=0)
-                    pull_std = np.std(pull, axis=0)
-
-                    # rather roughly speaking, we expect pull to be gaussian about 0 with std of 1
-                    np.testing.assert_allclose(pull_mean, np.zeros(3), atol=0.2)
-                    np.testing.assert_allclose(pull_std, np.ones(3), atol=0.2)
-
 if __name__ == '__main__':
     test_simple()
     test_center()
@@ -733,4 +689,3 @@ if __name__ == '__main__':
     test_gradient()
     test_gradient_center()
     test_direct()
-    test_lmfit_errors()
