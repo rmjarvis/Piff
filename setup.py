6e1242ff92d59e78ec2ed8a45ab0a9ed6b4d53af
Mike Jarvis <michael@jarvis.net>
2021-09-30 18:50:48 -0400

    Actually, these minimum versions are more appropriate

--- setup.py
+++ setup.py
@@ -283,7 +283,7 @@ ext=Extension("piff._piff",
               depends=headers,
               undef_macros = undef_macros)
 
-dependencies = ['galsim>=2.3', 'numpy>=1.15', 'scipy>=1.1', 'pyyaml>=5.1', 'treecorr>=4.0', 'fitsio>=1.0', 'matplotlib>=3.3', 'LSSTDESC.Coord>=1.0', 'treegp>=0.6']
+dependencies = ['galsim>=2.3', 'numpy>=1.17', 'scipy>=1.2', 'pyyaml>=5.1', 'treecorr>=4.2', 'fitsio>=1.0', 'matplotlib>=3.3', 'LSSTDESC.Coord>=1.0', 'treegp>=0.6']
 
 with open('README.rst') as file:
     long_description = file.read()
