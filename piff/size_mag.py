d1f7e64b11bb16d2732d2332a6f3814e6b7907d3
Mike Jarvis <michael@jarvis.net>
2021-10-07 23:32:51 -0400

    Add SmallBright selection

--- piff/size_mag.py
+++ piff/size_mag.py
@@ -19,6 +19,7 @@ import galsim
 # This file include both the SizeMagSelect selection type and the SizeMagStats stat type.
 
 from .stats import Stats
+from .select import Select
 
 class SizeMagStats(Stats):
     """Statistics class that plots a size magnitude diagram to check the quality of
@@ -141,3 +142,145 @@ class SizeMagStats(Stats):
               loc='lower left', frameon=True, fontsize=15)
 
         return fig, ax
+
+
+class SmallBrightSelect(Select):
+    """A Select handler that picks stars by looking for a locus of small, bright objects that
+    are all about the same size.
+
+    This is a very crude selection.  It is typically used to provide an initial selection to
+    the SizeMag selector.
+    """
+    def __init__(self, config, logger=None):
+        """
+        Parse the config dict (Normally the 'where' field in the overall configuration dict).
+
+        The following parameters are tunable in the config field, but they all have reasonable
+        default values:
+
+            :bright_fraction:   The fraction of the detected objects to consider "bright" and
+                                thus available for consideration for star selection.
+                                [default: 0.2]
+            :small_fraction:    The fraction of the bright objects to consider "small".
+                                [default: 0.2]
+            :locus_fraction:    The fraction of the initial small, bright objects to look for
+                                having all nearly the same size.  These constitude the first
+                                extremely crude selection of high-confidence stars. [default: 0.5]
+            :max_spread:        The maximum spread in log(T) to allow for the final set of stars.
+                                From the initial high-confidence stars, we expand the range in
+                                size to try to capture the full stellar locus based on the
+                                inter-quartile range of the candidate stars.  This parameter sets
+                                a maximum range to allow for this locus to make sure it doesn't
+                                erroneously expand to include everything. [default: 0.1]
+
+        :param config:      The configuration dict used to define the above parameters.
+        :param logger:      A logger object for logging debug info. [default: None]
+        """
+        super(SmallBrightSelect, self).__init__(config, logger)
+
+        opt = {
+            'bright_fraction': float,
+            'small_fraction': float,
+            'locus_fraction': float,
+            'max_spread': float,
+        }
+        params = galsim.config.GetAllParams(config, config, opt=opt, ignore=Select.base_keys)[0]
+        self.bright_fraction = params.get('bright_fraction', 0.2)
+        self.small_fraction = params.get('small_fraction', 0.2)
+        self.locus_fraction = params.get('locus_fraction', 0.5)
+        self.max_spread = params.get('max_spread', 0.1)
+
+    def selectStars(self, objects, logger=None):
+        """Select which of the input objects should be considered stars.
+
+        :param objects:     A list of input objects to be considered as potential stars.
+        :param logger:      A logger object for logging debug info. [default: None]
+
+        :returns: a list of Star instances
+        """
+        logger = galsim.config.LoggerWrapper(logger)
+
+        logger.info("Selecting small/bright objects as stars")
+
+        logger.debug("Initial count = %s", len(objects))
+
+        # Get size, flux from hsm
+        obj_shapes = np.array([ obj.hsm for obj in objects ])
+        flag_obj = obj_shapes[:, 6]
+        f_obj = obj_shapes[:, 0]
+        T_obj = obj_shapes[:, 3]
+
+        # Getting rid of the flags will mess with the indexing, so keep track of the original
+        # index numbers.
+        mask = flag_obj == 0
+        orig_index = np.arange(len(objects))[mask]
+
+        # Work in log/log space.
+        # log(f) is basically a magnitude with different spacing.
+        # size of stars is constant, so log(T) doesn't matter that much, but it means that
+        # the width of the locus in the size direction is really a fractional width.  This is
+        # nice because it gets rid of any scaling issues due to units or pixel size, etc.
+        logf = np.log(f_obj[mask])
+        logT = np.log(T_obj[mask])
+        logger.debug("After removing flags count = %s", len(logf))
+
+        # Pick out brightest 20% (or bright_fraction if given)
+        i20 = int(np.ceil(len(logf) * self.bright_fraction))
+        sort_index = np.argpartition(-logf, i20)
+        bright_logf = logf[sort_index[:i20]]
+        bright_logT = logT[sort_index[:i20]]
+        logger.debug("Bright objects:")
+        logger.debug("logf = %s", bright_logf)
+        logger.debug("logT = %s", bright_logT)
+
+        # Now take smallest 20% of these (or small_fraction if given)
+        i20 = int(np.ceil(len(bright_logf) * self.small_fraction))
+        sort_index = np.argpartition(bright_logT, i20)
+        bright_small_logf = bright_logf[sort_index[:i20]]
+        bright_small_logT = bright_logT[sort_index[:i20]]
+        logger.debug("Bright/small objects:")
+        logger.debug("logf = %s", bright_small_logf)
+        logger.debug("logT = %s", bright_small_logT)
+
+        # Sort these by size
+        sort_index = np.argsort(bright_small_logT)
+        bright_small_logf = bright_small_logf[sort_index]
+        bright_small_logT = bright_small_logT[sort_index]
+
+        # Find the "half" with the smallest range in size
+        half_len = int(np.ceil(len(bright_small_logT) * self.locus_fraction))
+        delta_T = bright_small_logT[-half_len-1:] - bright_small_logT[:half_len+1]
+        imin = np.argmin(delta_T[:-half_len])
+        star_logT = bright_small_logT[imin:imin+half_len]
+        logger.info("Initial bright/small selection includes %d objects",half_len)
+
+        # Expand this to include all stars that are within twice the interquarile range of
+        # these candidate stars.  Keep doing so until we converge on a good set of stars.
+        old_select = None  # Force at least 2 iterations.
+        while True:
+            logger.debug("Sizes of candidate stars = %s", np.exp(star_logT))
+            med = np.median(star_logT)
+            logger.info("Median size = %s", np.exp(med))
+            q25, q75 = np.percentile(star_logT, [25,75])
+            iqr = q75 - q25
+            logger.debug("Range of initial star logT size = %s, %s",
+                        np.min(star_logT), np.max(star_logT))
+            logger.debug("IQR = %s",iqr)
+            iqr = min(iqr,self.max_spread/4)
+            select = (logT > med - 2*iqr) & (logT < med + 2*iqr) & (logf > np.min(bright_logf))
+            new_count = np.sum(select)
+            # Break out when we stop adding more stars.
+            if np.array_equal(select, old_select):
+                break
+            old_select = select
+            logger.info("Expand this to include %d selected stars",new_count)
+            star_logT = logT[select]
+
+        # Get the initial indexes of these objects
+        select_index = orig_index[select]
+        logger.debug("select_index = %s",select_index)
+        stars = [objects[i] for i in select_index]
+        logger.debug("sizes of stars = %s",[s.hsm[3] for s in stars])
+        logger.debug("fluxs of stars = %s",[s.hsm[0] for s in stars])
+
+        return stars
--- tests/test_sizemag.py
+++ tests/test_sizemag.py
@@ -50,6 +50,86 @@ def test_sizemag_plot():
     piff.plotify(config, logger)
     assert os.path.isfile(file_name)
 
+@timer
+def test_smallbright():
+    """Test the SmallBright selection algorithm.
+    """
+    if __name__ == '__main__':
+        logger = piff.config.setup_logger(2)
+    else:
+        #logger = piff.config.setup_logger(2)
+        logger = None
+
+    config = piff.config.read_config('sizemag.yaml')
+    config['select'] = {
+        'type': 'SmallBright'
+    }
+
+    objects, _, _ = piff.Input.process(config['input'], logger=logger)
+    stars = piff.Select.process(config['select'], objects, logger=logger)
+
+    # This does a pretty decent job actually.  Finds 94 stars.
+    print('nstars = ',len(stars))
+    assert len(stars) == 94
+
+    # They are all ones that CLASS_STAR also identified as stars.
+    class_star = np.array([s['CLASS_STAR'] for s in stars])
+    print('class_star = ',class_star)
+    print('min class_star = ',np.min(class_star))
+    assert np.all(class_star > 0.95)
+
+    # Sizes are all pretty similar (by construction of the algorithm)
+    sizes = [s.hsm[3] for s in stars]
+    print('mean size = ',np.mean(sizes))
+    print('median size = ',np.median(sizes))
+    print('min/max size = ',np.min(sizes),np.max(sizes))
+    assert (np.max(sizes)-np.min(sizes)) / np.median(sizes) < 0.1
+
+    # Try some different parameter values.
+    config['select'] = {
+        'type': 'SmallBright',
+        'bright_fraction': 0.1,
+        'small_fraction': 0.5,
+        'locus_fraction': 0.8,
+        'max_spread': 0.05,
+    }
+    stars = piff.Select.process(config['select'], objects, logger=logger)
+
+    # Fewer stars since limited to brighter subset
+    print('nstars = ',len(stars))
+    assert len(stars) == 51
+
+    # But still finds all high confidence stars
+    class_star = np.array([s['CLASS_STAR'] for s in stars])
+    print('class_star = ',class_star)
+    print('min class_star = ',np.min(class_star))
+    assert np.all(class_star > 0.95)
+
+    # And sizes are now restricted to max_spread = 0.05
+    sizes = [s.hsm[3] for s in stars]
+    print('mean size = ',np.mean(sizes))
+    print('median size = ',np.median(sizes))
+    print('min/max size = ',np.min(sizes),np.max(sizes))
+    assert (np.max(sizes)-np.min(sizes)) / np.median(sizes) < 0.05
+
+    # Error to have other parameters
+    config['select'] = {
+        'type': 'SmallBright',
+        'bright_frac': 0.1,
+        'small_frac': 0.5,
+    }
+    with np.testing.assert_raises(ValueError):
+        piff.Select.process(config['select'], objects, logger=logger)
+
+    # But ok to have parameters that the base class will handle.
+    config['select'] = {
+        'type': 'SmallBright',
+        'locus_fraction': 0.8,
+        'hsm_size_reject': 4,
+        'min_snr': 50,
+    }
+    piff.Select.process(config['select'], objects, logger=logger)
+
 
 if __name__ == '__main__':
     test_sizemag_plot()
