430a4b2741b07bf93220cf81920f60e74fa46398
Mike Jarvis <michael@jarvis.net>
2021-10-16 14:28:33 -0400

    Use fixed g1,g2,size for test_moments_errors so empirical var is right.

--- tests/test_moments.py
+++ tests/test_moments.py
@@ -17,14 +17,15 @@ import numpy as np
 import piff
 import galsim
 import copy
+import os
 
 from piff.util import calculate_moments
 from piff_test_helper import timer
 
-def makeStars(nstar, beta):
+def makeStars(nstar, beta, size=None, g1=None, g2=None, seed=12345):
 
-    rng = galsim.BaseDeviate(12345)
-    np_rng = np.random.default_rng(12345)
+    rng = galsim.BaseDeviate(seed)
+    np_rng = np.random.default_rng(seed)
     flux = 1.e6
     sky_level = 200.0  # For noise
 
@@ -32,14 +33,19 @@ def makeStars(nstar, beta):
     decaminfo = piff.des.DECamInfo()
     wcs = decaminfo.get_nominal_wcs(chipnum=10)
 
-    # set the pixel index randomly x,y
-    x = np_rng.uniform(0,2048,nstar)
-    y = np_rng.uniform(0,4096,nstar)
-
-    # pick random size, shape
-    size = np_rng.uniform(0.7,1.2,nstar)
-    g1 = np_rng.uniform(-0.1, 0.1, nstar)
-    g2 = np_rng.uniform(-0.1, 0.1, nstar)
+    # pick random size, shape if not given
+    if size is None:
+        size = np_rng.uniform(0.5, 0.9, nstar)
+    else:
+        size = np.array([size]*nstar)
+    if g1 is None:
+        g1 = np_rng.uniform(-0.1, 0.1, nstar)
+    else:
+        g1 = np.array([g1]*nstar)
+    if g2 is None:
+        g2 = np_rng.uniform(-0.1, 0.1, nstar)
+    else:
+        g2 = np.array([g2]*nstar)
 
     noiseless_stars = []
     noisy_stars = []
@@ -50,7 +56,7 @@ def makeStars(nstar, beta):
         prof = galsim.Moffat(half_light_radius=size[i], beta=beta).shear(g1=g1[i], g2=g2[i])
 
         # make the star with no noise.
-        noiseless_star = piff.Star.makeTarget(x=x[i], y=y[i], wcs=wcs, stamp_size=19)
+        noiseless_star = piff.Star.makeTarget(x=0, y=0, wcs=wcs, stamp_size=19)
         prof = prof.shift(noiseless_star.fit.center).withFlux(flux)
         im = noiseless_star.image
         prof.drawImage(image=im, center=noiseless_star.image_pos)
@@ -98,7 +104,7 @@ def test_moments_mask():
 
         copy_star = copy.deepcopy(star)
         # mask out a pixel that is not too close to the center of the stamp.
-        copy_star.data.weight[star.image_pos.x+5, star.image_pos.y+5] = 0
+        copy_star.weight[5, 5] = 0
         test_moments = calculate_moments(copy_star, errors=True,
                                          third_order=True, fourth_order=True, radial=True)
         # make sure that they did actually change
@@ -171,42 +177,88 @@ def test_moments_fail():
 @timer
 def test_moments_errors():
 
-    betalist = [1.5, 2.5, 5.]
+    ntests = 10
+    np_rng = np.random.default_rng(1234)
 
+    # Test N different stars with random choices for beta, g1, g2, size within reasonable ranges.
+    betalist = np_rng.uniform(1.5, 6, size=ntests)
+    g1list = np_rng.uniform(-0.1, 0.1, size=ntests)
+    g2list = np_rng.uniform(-0.1, 0.1, size=ntests)
+    sizelist = np_rng.uniform(0.5, 0.9, size=ntests)
+
+    # The error estimates aren't super precise.
+    # Here are the tolerances we need to get these tests to pass.  (To the nearest 0.05.)
     tol_dict = dict(M00=2.00,
                     M10=0.10, M01=0.10,
-                    M11=0.35, M20=0.15, M02=0.15,
-                    M21=0.10, M12=0.10, M30=0.10, M03=0.10,
-                    M22=0.50, M31=0.30, M13=0.30, M40=0.30, M04=0.30,
-                    M22n=0.15, M33n=0.20, M44n=0.20)
+                    M11=0.35, M20=0.20, M02=0.20,
+                    M21=0.15, M12=0.15, M30=0.10, M03=0.10,
+                    M22=0.60, M31=0.40, M13=0.40, M40=0.35, M04=0.35,
+                    M22n=0.20, M33n=0.25, M44n=0.25)
 
-    for beta in betalist:
-        noiseless_stars, noisy_stars = makeStars(nstar=1000, beta=beta)
+    for k in range(ntests):
+        # Use a single value of beta, g1, g2, size for each run so empirical variance is accurate.
+        beta = betalist[k]
+        g1 = g1list[k]
+        g2 = g2list[k]
+        size = sizelist[k]
+        print('beta, g1, g2, size = ',beta,g1,g2,size)
 
-        noiseless_moments = []
-        noisy_moments = []
-        var_moments = []
-        for noiseless_star, noisy_star in zip(noiseless_stars, noisy_stars):
-            moments = calculate_moments(noiseless_star, errors=False,
-                                        third_order=True, fourth_order=True, radial=True)
-            moments_noise = calculate_moments(noisy_star, errors=True,
-                                              third_order=True, fourth_order=True, radial=True)
-            noiseless_moments.append(moments)
-            noisy_moments.append(moments_noise[:18])
-            var_moments.append(moments_noise[18:])
+        # Calculate empirical variances from 1000 noise realizations.
+        # These are saved in a file in the repo.  To regenerate, just delete the file.
+        file_name = f'input/test_moments_errors_{beta:.2f}_{g1:.2f}_{g2:.2f}_{size:.2f}.npz'
+        print(file_name)
+        if not os.path.isfile(file_name):
+            noiseless_moments = []
+            noisy_moments = []
+            var_moments = []
+            noiseless_stars, noisy_stars = makeStars(nstar=1000, seed=31415,
+                                                     beta=beta, g1=g1, g2=g2, size=size)
+            for noiseless_star, noisy_star in zip(noiseless_stars, noisy_stars):
+                moments = calculate_moments(noiseless_star, errors=False,
+                                            third_order=True, fourth_order=True, radial=True)
+                moments_noise = calculate_moments(noisy_star, errors=True,
+                                                third_order=True, fourth_order=True, radial=True)
+                noiseless_moments.append(moments)
+                noisy_moments.append(moments_noise[:18])
+                var_moments.append(moments_noise[18:])
 
-        # Transpose from arrays of moments by star to array of values by moment name
-        noiseless_moments = np.column_stack(noiseless_moments)
-        noisy_moments = np.column_stack(noisy_moments)
-        var_moments = np.column_stack(var_moments)
+            # Transpose from arrays of moments by star to array of values by moment nameI
+            noiseless_moments = np.column_stack(noiseless_moments)
+            noisy_moments = np.column_stack(noisy_moments)
+            var_moments = np.column_stack(var_moments)
 
+            np.savez(file_name,
+                     noiseless_moments=noiseless_moments,
+                     noisy_moments=noisy_moments,
+                     var_moments=var_moments)
+
+        data = np.load(file_name)
+        noiseless_moments = data['noiseless_moments']
+        noisy_moments = data['noisy_moments']
+        var_moments = data['var_moments']
+
+        # Check that the pulls seem reasonable.
         for k, name in enumerate(tol_dict.keys()):
             diff = noisy_moments[k] - noiseless_moments[k]
-            pull = diff / np.sqrt(var_moments[k])
-            print("beta = {}, {}: mean, rms pull = {:0.2f}, {:0.2f}".format(
-                    beta, name, np.mean(pull), np.std(pull)))
-            np.testing.assert_allclose(np.mean(pull), 0., atol=0.1)
+            pull = diff / np.sqrt(np.mean(var_moments[k]))
+            print("{}: mean, rms pull = {:0.2f}, {:0.2f}".format(
+                    name, np.mean(pull), np.std(pull)))
+            np.testing.assert_allclose(np.mean(pull), 0., atol=0.2)
             np.testing.assert_allclose(np.std(pull), 1., atol=tol_dict[name])
+
+        # Now test a single star to check that the error estimates match the empirical estimates.
+        _, noisy_stars = makeStars(nstar=1, beta=beta, g1=g1, g2=g2, size=size)
+        star = noisy_stars[0]
+        moments = calculate_moments(star, errors=True,
+                                    third_order=True, fourth_order=True, radial=True)
+        for k, name in enumerate(tol_dict.keys()):
+            mom = moments[k]
+            var = moments[k+18]
+            print("{}: mom, var = {:0.2f}, {:0.2e}  cf. {:0.2f}, {:0.2e}".format(
+                    name, mom, var, np.mean(noisy_moments[k]), np.var(noisy_moments[k])))
+            np.testing.assert_allclose(mom, np.mean(noisy_moments[k]), rtol=0.2, atol=0.01)
+            # Note: 2x tol since var now rather than std.
+            np.testing.assert_allclose(var, np.var(noisy_moments[k]), rtol=2*tol_dict[name])
 
 
 if __name__ == "__main__":
