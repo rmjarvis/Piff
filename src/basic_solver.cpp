e10a50527b8f22ad4e2baf984ef624196dba8cea
Pierre-Francois Leget <leget@astro.princeton.edu>
2024-10-21 14:52:00 -0700

    avoid throw error in case above 10

--- src/basic_solver.cpp
+++ src/basic_solver.cpp
@@ -9,13 +9,13 @@
 namespace py = pybind11;
 
 
-// TO DO: template<class T> //, ssize_t N>
 template<class T, ssize_t N>
 auto _solve_direct_cpp_impl(
         std::vector<py::array_t<T, py::array::f_style>> bList,
         std::vector<py::array_t<T, py::array::f_style>> AList,
-        std::vector<py::array_t<T, py::array::f_style>> KList) {
-        // TO DO: Add this instead--> ssize_t N) {
+        std::vector<py::array_t<T, py::array::f_style>> KList,
+        ssize_t n) {
+
     // get size info
     py::buffer_info b_buffer_info = bList[0].request();
     py::buffer_info A_buffer_info = AList[0].request();
@@ -26,25 +26,25 @@ auto _solve_direct_cpp_impl(
     // Allocate and initialize container array for the A transpose A matrix.
     // For some reason it is faster to allocate a numpy array and then map
     // it to Eigen than allocating an Eigen array directly
-    py::array_t<T, py::array::f_style> ATA(std::vector<ssize_t>({A_shape[1]*N, A_shape[1]*N}));
+    py::array_t<T, py::array::f_style> ATA(std::vector<ssize_t>({A_shape[1]*n, A_shape[1]*n}));
     py::buffer_info ATA_buffer = ATA.request();
     T __restrict * ATA_data = static_cast<T *>(ATA_buffer.ptr);
     // set it all to zero
-    memset(ATA_data, 0, A_shape[1]*N*A_shape[1]*N*sizeof(T));
+    memset(ATA_data, 0, A_shape[1]*n*A_shape[1]*n*sizeof(T));
     Eigen::Map< Eigen::Matrix<T,
                               Eigen::Dynamic,
                               Eigen::Dynamic,
-                              Eigen::ColMajor> > ATA_eig(ATA_data, A_shape[1]*N, A_shape[1]*N);
+                              Eigen::ColMajor> > ATA_eig(ATA_data, A_shape[1]*n, A_shape[1]*n);
 
     // Allocate an array for b, similar to ATA
-    auto ATb = py::array_t<T>(A_shape[1]*N);
+    auto ATb = py::array_t<T>(A_shape[1]*n);
     py::buffer_info ATb_buffer = ATb.request();
     T __restrict * ATb_data = static_cast<T *>(ATb_buffer.ptr);
-    memset(ATb_data, 0, A_shape[1]*N*sizeof(T));
+    memset(ATb_data, 0, A_shape[1]*n*sizeof(T));
 
     // Allocate intermediate arrays that will be reused in the following loops
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> ATA_small(A_shape[1],A_shape[1]);
-    Eigen::Matrix<T, N, N> K_cross(N, N);
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> K_cross(n, n);
     Eigen::Vector<T, Eigen::Dynamic> ATb_small(A_shape[1]);
 
     // loop over each Star
@@ -67,7 +67,7 @@ auto _solve_direct_cpp_impl(
 
         // calculate the outer product of the K parameters so that it can be re-used in the
         // following loops
-        Eigen::Map<const Eigen::Vector<T, N>> K_vec(K_data, N);
+        Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>> K_vec(K_data, n);
         K_cross = K_vec *K_vec.transpose();
 
         Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> A_map(A_data, A_shape_zero, A_shape_one);
@@ -82,27 +82,36 @@ auto _solve_direct_cpp_impl(
     
         // Calculate ATb contribution for this list entry
         for (ssize_t k = 0; k < A_shape_one; k ++) {
-            ssize_t base_pos = k*N;
-            for (ssize_t l = 0; l < N; l++) {
+            ssize_t base_pos = k*n;
+            for (ssize_t l = 0; l < n; l++) {
                 ATb_data[base_pos+l] += K_data[l] * ATb_small(k);
             }
         }
 
         // Calculate ATA contribution for this list entry
-        for (ssize_t j = 0; j < A_shape_one; j++) {
-            ssize_t base_pos_j = j*N;
-            for (ssize_t i = 0; i < j+1; i++) {
-                ssize_t base_pos = i*N;
-                ATA_eig.template block<N, N>(base_pos, base_pos_j) += ATA_small(i,j)*K_cross;
-                // TO DO: this is where it was supposed to make the difference because of block assignement.
-                // TO DO: ATA_eig.block(base_pos, base_pos_j, N, N)  += ATA_small(i,j)*K_cross;
+        if (n<11) {
+            for (ssize_t j = 0; j < A_shape_one; j++) {
+                ssize_t base_pos_j = j*N;
+                for (ssize_t i = 0; i < j+1; i++) {
+                    ssize_t base_pos = i*N;
+                    ATA_eig.template block<N, N>(base_pos, base_pos_j) += ATA_small(i,j)*K_cross;
+                }
+            }
+        }
+        else {
+            for (ssize_t j = 0; j < A_shape_one; j++) {
+                ssize_t base_pos_j = j*n;
+                for (ssize_t i = 0; i < j+1; i++) {
+                    ssize_t base_pos = i*n;
+                    ATA_eig.block(base_pos, base_pos_j, n, n) += ATA_small(i,j)*K_cross;
+                }
             }
         }
     }
 
     // solve Ax=b. If the matrix can be decomposed using llt do that (faster) if it cant be
     // then use the slower ldlt method
-    Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>> ATb_map(ATb_data, A_shape[1]*N);
+    Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>> ATb_map(ATb_data, A_shape[1]*n);
     auto view = ATA_eig. template selfadjointView<Eigen::Upper>();
     auto lltDecomp = view.llt();
     Eigen::Vector<T, Eigen::Dynamic> result;
@@ -142,33 +151,31 @@ auto _solve_direct_cpp(
 
     py::buffer_info K_buffer_info = KList[0].request();
 
-    // TO DO: return _solve_direct_cpp_impl<T>(bList, AList, KList,  K_buffer_info.shape[0]);
-
     // Switch on the length of the K parameter, this templated value
     // allows the compiler to make various optimizations
     switch (K_buffer_info.shape[0]) {
         case 1:
-            return _solve_direct_cpp_impl<T, 1>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 1>(bList, AList, KList, K_buffer_info.shape[0]);
         case 2:
-            return _solve_direct_cpp_impl<T, 2>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 2>(bList, AList, KList, K_buffer_info.shape[0]);
         case 3:
-            return _solve_direct_cpp_impl<T, 3>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 3>(bList, AList, KList, K_buffer_info.shape[0]);
         case 4:
-            return _solve_direct_cpp_impl<T, 4>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 4>(bList, AList, KList, K_buffer_info.shape[0]);
         case 5:
-            return _solve_direct_cpp_impl<T, 5>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 5>(bList, AList, KList, K_buffer_info.shape[0]);
         case 6:
-            return _solve_direct_cpp_impl<T, 6>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 6>(bList, AList, KList, K_buffer_info.shape[0]);
         case 7:
-            return _solve_direct_cpp_impl<T, 7>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 7>(bList, AList, KList, K_buffer_info.shape[0]);
         case 8:
-            return _solve_direct_cpp_impl<T, 8>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 8>(bList, AList, KList, K_buffer_info.shape[0]);
         case 9:
-            return _solve_direct_cpp_impl<T, 9>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 9>(bList, AList, KList, K_buffer_info.shape[0]);
         case 10:
-            return _solve_direct_cpp_impl<T, 10>(bList, AList, KList);
+            return _solve_direct_cpp_impl<T, 10>(bList, AList, KList, K_buffer_info.shape[0]);
         default:
-            throw pybind11::value_error("Can only handle K with a side of length 1 to 10");
+            return _solve_direct_cpp_impl<T, 11>(bList, AList, KList, K_buffer_info.shape[0]);
     }
 }
 
